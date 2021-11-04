cab51080197f1aca140c6cc8eae5969ca7fb1188
lexreal25 <40786380+lexreal25@users.noreply.github.com>
2021-11-04 12:47:22 +0000

    made changes to the trade page

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "react-native";
+import COLORS from "./src/consts/colors";
+import { createStackNavigator } from "@react-navigation/stack";
+import { OnBoard } from "./src/screen/onBoard";
+import { Register } from "./src/components/Authentication/Registeration";
+import { Login } from "./src/components/Authentication/Login";
+import { ResetPassword } from "./src/components/Authentication/ResetPassword";
+import { Service } from "./src/screen/Service";
+import { Sell } from "./src/screen/Sell";
+import { Cart } from "./src/screen/Cart";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar backgroundColor={COLORS.primary} barStyle="light-content" />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="BoardScreen" component={OnBoard} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Reset" component={ResetPassword} />
+        <Stack.Screen name="Services" component={Service} />
+        <Stack.Screen name="Sell" component={Sell} />
+        <Stack.Screen name='Cart' component={Cart} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/Authentication/Login.jsx
@@ -0,0 +1,152 @@
+import { Ionicons } from "@expo/vector-icons";
+import React, { useState } from "react";
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import logo from "../../assets/logo.png";
+import COLORS from "../../consts/colors";
+import { DontHaveAcc } from "../General/DontHaveAcc";
+
+
+export const Login = ({ navigation }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(true);
+
+
+  return (
+    <SafeAreaView style={styles.loginContainer}>
+      <View
+        style={{
+          backgroundColor: COLORS.white,
+          borderRadius: 50,
+          padding: 10,
+          alignSelf: "flex-start",
+          margin: 5,
+        }}
+      >
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Ionicons
+            name="arrow-back-outline"
+            size={20}
+            color={COLORS.primary}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.login}>
+        <Image style={styles.logo} source={logo} alt="logo" />
+        <View>
+          <Text
+            style={{
+              fontSize: 20,
+              marginBottom: 20,
+              fontWeight: "bold",
+              color: COLORS.primary,
+            }}
+          >
+            Sign In Here
+          </Text>
+          <TextInput
+            placeholder="Username"
+            required="required"
+            placeholderTextColor={COLORS.primary}
+            underlineColorAndroid={"transparent"}
+            textDecorationLine="#fff"
+            style={styles.textLayout}
+            onChangeText={(username) => setUsername(username)}
+          />
+          <TextInput
+            placeholder="Password"
+            placeholderTextColor={COLORS.primary}
+            underlineColorAndroid={"transparent"}
+            style={styles.textLayout}
+            secureTextEntry={true}
+            onChangeText={(password) => setPassword(password)}
+          />
+        </View>
+        <View style={styles.resetPswd}>
+          <TouchableOpacity onPress={() => navigation.navigate("Reset")}>
+            <Text style={styles.resetPswdText}>Forgot Password?</Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={ user? () => navigation.navigate("Sell"): console.log("failed")}
+        >
+          <View style={styles.btnContainer}>
+            <Text style={styles.btnTxt}>LOGIN</Text>
+          </View>
+        </TouchableOpacity>
+        <DontHaveAcc onPress={() => navigation.navigate("Register")} />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  loginContainer: {
+    flex: 1,
+  },
+  login: {
+    flex: 1,
+    top: -15,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+
+    elevation: 8,
+  },
+  logo: {
+    width: 400,
+    height: 150,
+  },
+  textLayout: {
+    fontSize: 16,
+    alignSelf: "stretch",
+    height: 50,
+    borderRadius: 5,
+    width: 270,
+    marginBottom: 20,
+    paddingLeft: 5,
+    borderWidth: 0.5,
+    borderColor: COLORS.primary,
+    textAlign: "center",
+    color: COLORS.primary,
+    backgroundColor: COLORS.silver,
+  },
+  btnContainer: {
+    backgroundColor: COLORS.primary,
+    height: 50,
+    width: 270,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  btnTxt: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: COLORS.white,
+  },
+  resetPswd: {
+    alignSelf: "flex-end",
+    marginRight: 25,
+  },
+  resetPswdText: {
+    fontSize: 12,
+    color: COLORS.primary,
+    textDecorationLine: "underline",
+  },
+});
